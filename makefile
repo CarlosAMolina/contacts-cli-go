61a7a70e9ac776674b265ab168eef8e53f1f7797
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-01-10 20:31:44 +0100

    feat(main): search term as user input

--- makefile
+++ makefile
@@ -5,7 +5,7 @@ fmt:
 	go fmt ./src/...
 
 run:
-	go run src/main.go src/types.go
+	go run src/types.go src/search.go src/main.go 234
 
 tidy:
 	go mod tidy
--- src/main.go
+++ src/main.go
@@ -7,15 +7,26 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <searchTerm>")
+		os.Exit(1)
+	}
+
+	searchTerm := os.Args[1]
+
 	jsonFile, err := os.ReadFile("src/fake.json")
 	if err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	var data Data
-	json.Unmarshal(jsonFile, &data)
+	err = json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		os.Exit(1)
+	}
 
-	searchTerm := "foo"
 	matchedIDs := searchContacts(searchTerm, data)
 	fmt.Printf("Contacts matching '%s': %v\n", searchTerm, matchedIDs)
 }
