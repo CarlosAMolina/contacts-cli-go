ca8c20b56f4bda70224d847cdd7d36601b21afe0
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-01-10 22:08:32 +0100

    refactor(search): move line

--- src/search.go
+++ src/search.go
@@ -25,6 +25,33 @@ func searchContacts(term string, contacts []Contact) []Contact {
 	return matchedContacts
 }
 
+func getSummary(contact Contact) []string {
+	var result []string
+	name := contact.Name
+	if contact.Surname != "" {
+		name = name + " " + contact.Surname
+	}
+	nicknamesStr := ""
+	if len(contact.Nicknames) > 0 {
+		nicknamesStr = ". " + strings.Join(contact.Nicknames, ", ")
+	}
+	categoriesStr := ""
+	if len(contact.Categories) > 0 {
+		categoriesStr = strings.Join(contact.Categories, ", ")
+		categoriesStr = strings.ToUpper(string(categoriesStr[0])) + categoriesStr[1:]
+		categoriesStr = ". " + categoriesStr
+	}
+	for _, phone := range contact.Phones {
+		description := phone.Description
+		if description != "" {
+			description = strings.ToUpper(string(description[0])) + description[1:]
+			description = "(" + description + ") "
+		}
+		result = append(result, fmt.Sprintf("%s %s%s%s%s. ID %d", phone.Number, description, name, nicknamesStr, categoriesStr, contact.ID))
+	}
+	return result
+}
+
 func searchInValue(v reflect.Value, term string) bool {
 	switch v.Kind() {
 	case reflect.String:
@@ -51,31 +78,4 @@ func searchInValue(v reflect.Value, term string) bool {
 		}
 	}
 	return false
-}
-
-func getSummary(contact Contact) []string {
-	var result []string
-	name := contact.Name
-	if contact.Surname != "" {
-		name = name + " " + contact.Surname
-	}
-	nicknamesStr := ""
-	if len(contact.Nicknames) > 0 {
-		nicknamesStr = ". " + strings.Join(contact.Nicknames, ", ")
-	}
-	categoriesStr := ""
-	if len(contact.Categories) > 0 {
-		categoriesStr = strings.Join(contact.Categories, ", ")
-		categoriesStr = strings.ToUpper(string(categoriesStr[0])) + categoriesStr[1:]
-		categoriesStr = ". " + categoriesStr
-	}
-	for _, phone := range contact.Phones {
-		description := phone.Description
-		if description != "" {
-			description = strings.ToUpper(string(description[0])) + description[1:]
-			description = "(" + description + ") "
-		}
-		result = append(result, fmt.Sprintf("%s %s%s%s%s. ID %d", phone.Number, description, name, nicknamesStr, categoriesStr, contact.ID))
-	}
-	return result
 }
