8cf2059f703776855590dee6d8e54f8fdef34ad9
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-01-10 21:10:22 +0100

    refactor(main): extract file data.go

---
+++ src/data.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func getData(path string) Data {
+	jsonFile, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	var data Data
+	err = json.Unmarshal(jsonFile, &data)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		os.Exit(1)
+	}
+	return data
+}
--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
 )
@@ -17,18 +16,7 @@ func main() {
 	const jsonPath = "src/fake.json"
 	//const jsonPath= "/tmp/contacts.json"
 
-	jsonFile, err := os.ReadFile(jsonPath)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	var data Data
-	err = json.Unmarshal(jsonFile, &data)
-	if err != nil {
-		fmt.Println("Error unmarshalling JSON:", err)
-		os.Exit(1)
-	}
+	var data Data = getData(jsonPath)
 
 	matchedIDs := searchContacts(searchTerm, data)
 	fmt.Printf("Found %d contacts matching '%s'\n", len(matchedIDs), searchTerm)
