2cd9feafe5610eb98ac307cb6dd765f52b131d94
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-02-02 21:05:50 +0100

    refactor(main): move code

--- src/main.go
+++ src/main.go
@@ -5,17 +5,6 @@ import (
 	"os"
 )
 
-func processAll(data Data) {
-	fmt.Printf("Number of contacts: %d\n", len(data.Contacts))
-	showSummary(data.Contacts)
-}
-
-func processTerm(data Data, searchTerm string) {
-	matchedContacts := searchContacts(searchTerm, data.Contacts)
-	fmt.Printf("Found %d contacts matching '%s'\n", len(matchedContacts), searchTerm)
-	showSummary(matchedContacts)
-}
-
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Println("Usage: go run main.go <jsonPath> <searchTerm>")
@@ -31,3 +20,15 @@ func main() {
 	searchTerm := os.Args[2]
 	processTerm(data, searchTerm)
 }
+
+func processAll(data Data) {
+	fmt.Printf("Number of contacts: %d\n", len(data.Contacts))
+	showSummary(data.Contacts)
+}
+
+func processTerm(data Data, searchTerm string) {
+	matchedContacts := searchContacts(searchTerm, data.Contacts)
+	fmt.Printf("Found %d contacts matching '%s'\n", len(matchedContacts), searchTerm)
+	showSummary(matchedContacts)
+}
+
