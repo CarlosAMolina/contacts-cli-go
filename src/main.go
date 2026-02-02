cab04dc4709ac5ce1b6e5047495ad1ba7bc70f96
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2026-02-02 21:47:47 +0100

    feat(main): add cli options and infinite loop

--- src/main.go
+++ src/main.go
@@ -6,21 +6,50 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <jsonPath> <searchTerm>")
-		fmt.Println("Example: go run main.go /tmp/contacts.json Carlos")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <contactsJsonPath>")
+		fmt.Println("Example: go run main.go /tmp/contacts.json")
 		os.Exit(1)
 	}
-
 	jsonPath := os.Args[1]
-
 	var data Data = readFile(jsonPath)
-	processAll(data)
+	fmt.Println("Welcome to the contacts CLI!")
+	showHelp()
+	var choice string
+	for {
+		fmt.Print(">> ")
+		fmt.Scan(&choice)
+		switch choice {
+		case "1":
+			processTerm(data)
+			fmt.Println()
+			showHelp()
+		case "2":
+			processId(data)
+			fmt.Println()
+			showHelp()
+		case "3":
+			processAll(data)
+			fmt.Println()
+			showHelp()
+		case "e":
+			fmt.Println("Bye!")
+			os.Exit(0)
+		case "h":
+			showHelp()
+		default:
+			fmt.Println("Invalid input")
+		}
+	}
+}
 
-	searchTerm := os.Args[2]
-	processTerm(data, searchTerm)
-
-	showContact(1, data.Contacts)
+func showHelp() {
+	fmt.Println("Please select an option:")
+	fmt.Println("1. Search term")
+	fmt.Println("2. Show contact by ID")
+	fmt.Println("3. Show all contacts")
+	fmt.Println("e. Exit")
+	fmt.Println("h. Show help")
 }
 
 func processAll(data Data) {
@@ -28,7 +57,25 @@ func processAll(data Data) {
 	showSummary(data.Contacts)
 }
 
-func processTerm(data Data, searchTerm string) {
+func processId(data Data) {
+	fmt.Println("Write the ID to show:")
+	fmt.Print(">> ")
+	var id int
+	_, err := fmt.Scan(&id)
+	for err != nil {
+		fmt.Println("Write a number")
+		fmt.Print(">> ")
+		_, err = fmt.Scan(&id)
+
+	}
+	showContact(id, data.Contacts)
+}
+
+func processTerm(data Data) {
+	fmt.Println("Write the term to search:")
+	fmt.Print(">> ")
+	var searchTerm string
+	fmt.Scan(&searchTerm)
 	matchedContacts := searchContacts(searchTerm, data.Contacts)
 	fmt.Printf("Found %d contacts matching '%s'\n", len(matchedContacts), searchTerm)
 	showSummary(matchedContacts)
